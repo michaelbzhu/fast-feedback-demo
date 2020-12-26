b39c70590e380eebb27fe630380056d0b5eba6e8
michaelbzhu <mbzhu1@gmail.com>
2020-12-26 15:52:14 -0800

    firestore and chakra

---
+++ styles/theme.js
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const themeExtension = {
+  fonts: {
+    body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    large: 700
+  }
+};
+
+const fonts = {
+  body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+};
+
+const fontWeights = {
+  normal: 400,
+  medium: 600,
+  large: 700
+};
+
+const customTheme = extendTheme({ fonts, fontWeights });
+
+export default customTheme;
