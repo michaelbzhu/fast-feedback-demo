3a62d3f0946b259f69fe2e4a22e83892db4519b8
michaelbzhu <mbzhu1@gmail.com>
2020-12-26 20:51:51 -0800

    more chakra stuff

--- styles/theme.js
+++ styles/theme.js
@@ -1,16 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const themeExtension = {
-  fonts: {
-    body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
-  },
-  fontWeights: {
-    normal: 400,
-    medium: 600,
-    large: 700
-  }
-};
-
 const fonts = {
   body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
 };
